d963c7aadffbb57e71dd022b7e749ebbe7b065f0
Ivannikov Roman <shark71@inbox.ru>
2023-07-08 14:15:52 +0300

    Решил четвертую задачу

---
+++ dz_1/4/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+
+Console.Write("Введите колличество чисел: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + number);
+int i = 1;                                             
+bool not = true;
+while (i <= number)
+    {
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+    }
+
+if (not)
+{
+     Console.WriteLine("Нет чётных чисел!");
+}
