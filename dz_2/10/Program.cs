5f22117727a2bcdd1c5f1df597618f788b5d6d82
Ivannikov Roman <shark71@inbox.ru>
2023-07-10 11:23:25 +0300

    Решил 10 задачу

--- dz_2/10/Program.cs
+++ dz_2/10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number < 1000){
+    int result = (number / 10) % 10;
+    Console.WriteLine("Вторая цыфра введеного числа " + result);
+}
+
+else{
+    Console.WriteLine("Число не трехзначное");
+}
+
+
+
