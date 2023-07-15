c08ca4703822148892b7e85de2e5898f1951ebc2
Ivannikov Roman <shark71@inbox.ru>
2023-07-15 14:14:22 +0300

    Решил 23 задачу

---
+++ dz_3/23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int start = 1;                                     // счетчик начинаем с 1 по условию
+if(number < 0)                                     // если число ввели отрицательное переводим в положительное
+{
+    number = -number;
+}
+while(start <= number)                             // выводим кубы начиная с 1 и заканчивая введеной цифрой
+{
+    int cube = start * start * start;
+    start ++;
+    System.Console.WriteLine(cube);
+}
