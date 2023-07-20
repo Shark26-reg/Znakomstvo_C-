be6f5dd0a3d3669cd6b462a80b1ff57b70517a0e
Ivannikov Roman <shark71@inbox.ru>
2023-07-20 17:34:51 +0300

    Решил 27 задачу

---
+++ dz_4/27/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num = Prompt("Введите число: ");
+int len = NumberLen(num);
+SumNumbers(num, len);
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while(a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+
+} 
+void SumNumbers(int number, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    System.Console.WriteLine($"Сумма цифр в числе {num} = {sum}");
+    
+}
+
+
+
