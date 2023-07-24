a47de4627cb27701adbe9dcd3074a2dd29144e8f
Ivannikov Roman <shark71@inbox.ru>
2023-07-24 22:03:00 +0300

    решил 41 задачу

--- dz_6/41/Program.cs
+++ dz_6/41/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] numbers = ParseStringToArray(Console.ReadLine());
+ 
+Console.WriteLine($"Введено чисел больше нуля -> {Comparison(numbers)} ");
+ 
+int Comparison(int[] Numbers) //сравнение введенных чисел
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if(Numbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+
+int[] ParseStringToArray(string input)
+{
+    int countNumbers = GetCountNumbersInString(input);
+    int[] numbers = new int[countNumbers];
+    int indexNumber = 0;
+    string substring = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ' ')
+        {
+            substring += input[i];
+        }
+        else
+        {
+            numbers[indexNumber] = Convert.ToInt32(substring);
+            indexNumber++;
+            substring = string.Empty;
+        }
+    }
+    if(input[input.Length - 1] != ' ')
+        numbers[indexNumber] = Convert.ToInt32(substring);
+    return numbers;
+}
+int GetCountNumbersInString(string input) 
+{
+    int count;
+    if(input[input.Length - 1] == ' ')
+        count = 0;
+    else
+        count = 1;
+
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i] == ' ')
+            count++;
+    }
+    return count;
+}
