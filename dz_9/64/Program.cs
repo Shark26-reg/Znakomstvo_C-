6497700f99b82922dc1f313c6748e2855b97c432
Ivannikov Roman <shark71@inbox.ru>
2023-08-06 21:02:44 +0300

    решил 64 задачу

---
+++ dz_9/64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+void PrintNumbers(int numberStart, int numberFinish)
+{
+    if (numberStart - 1 == numberFinish)
+    {
+        return;
+    }
+
+    System.Console.Write($"{numberFinish};");    
+    PrintNumbers(numberStart, numberFinish - 1);    
+}
+  
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int numberStart = 1;
+int numberFinish = Prompt("Введите число (конец промежутка) ");
+PrintNumbers(numberStart, numberFinish);
